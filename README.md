fbb87e2c9fd53905ed99cb530c2fdea635093b6c
Roy Westwater <rw547@cornell.edu>
2024-04-09 10:12:38 -0400

    Spring24 Updates

---
+++ README.md
@@ -0,0 +1,8 @@
+# Class-19-Activity-Cookie-Banner
+
+## Cookie Banner Implementation Notes
+
+Use the notes and snippets in the how-to.md to implement the cookie banner.
+
+
+
---
+++ scripts/cookie-banner.js
@@ -0,0 +1,7 @@
+/* When the accept button clicked, hide cookie banner. */
+$('#accept-cookies-button').click(function() {
+
+  /* hide cookie banner with CSS class. */
+  $('#cookie-banner').addClass('hidden');
+
+})
