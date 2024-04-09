5e45bcb6954c74b30495ff8c6cbdaf35f3650aaf
Roy Westwater <rw547@cornell.edu>
2024-04-09 10:30:12 -0400

    Spring24 Updates

--- scripts/cookie-banner.js
+++ scripts/cookie-banner.js
@@ -1,7 +1,2 @@
 /* When the accept button clicked, hide cookie banner. */
-$('#accept-cookies-button').click(function() {
 
-  /* hide cookie banner with CSS class. */
-  $('#cookie-banner').addClass('hidden');
-
-})
